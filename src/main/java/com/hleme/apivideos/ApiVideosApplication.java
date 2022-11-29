7e599efccf3f75d59356031f2c19278267fae78c
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-29 19:29:12 -0300

    ✨feat: first commit

---
+++ src/main/java/com/hleme/apivideos/ApiVideosApplication.java
@@ -0,0 +1,13 @@
+package com.hleme.apivideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVideosApplication.class, args);
+	}
+
+}
