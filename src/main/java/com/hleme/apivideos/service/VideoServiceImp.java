9a99ea285cf147052f8f862bef837f21d62915f2
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-30 11:49:07 -0300

    ✨feat: create service implement

---
+++ src/main/java/com/hleme/apivideos/service/VideoServiceImp.java
@@ -0,0 +1,9 @@
+package com.hleme.apivideos.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class VideoServiceImp implements VideoService {
+}
