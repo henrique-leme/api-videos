00bec0700d899c9f8860cbeaf11eb60f3e15f004
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:18:54 -0300

    ✨feat: add findAll method

--- src/main/java/com/hleme/apivideos/service/VideoService.java
+++ src/main/java/com/hleme/apivideos/service/VideoService.java
@@ -1,10 +1,11 @@
 package com.hleme.apivideos.service;
 
+import com.hleme.apivideos.DTO.response.VideoResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface VideoService {
 
-    Page<VideoResponse> findAll(Pageable pageable)
+    Page<VideoResponse> findAll(Pageable pageable);
 
 }
