8c66aab2247eeb4afe8b9b729dd7e58276e6ea2c
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:14:00 -0300

    ➕refactor: update video mapper implement

--- src/main/java/com/hleme/apivideos/mapper/VideoMapperImp.java
+++ src/main/java/com/hleme/apivideos/mapper/VideoMapperImp.java
@@ -1,4 +1,11 @@
 package com.hleme.apivideos.mapper;
 
-public class VideoMapperImp {
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class VideoMapperImp implements  VideoMapper{
+    private final ModelMapper mapper;
 }
