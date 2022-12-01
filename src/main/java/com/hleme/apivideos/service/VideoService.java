1930edbc2b83e416e4ee28b5961480033f0eca3d
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 19:58:16 -0300

    ✨feat: create findall video

--- src/main/java/com/hleme/apivideos/service/VideoService.java
+++ src/main/java/com/hleme/apivideos/service/VideoService.java
@@ -1,4 +1,10 @@
 package com.hleme.apivideos.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface VideoService {
+
+    Page<VideoResponse> findAll(Pageable pageable)
+
 }
