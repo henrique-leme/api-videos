1b3b67b85fce864deb8c028c4adf047cb2a36d01
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:20:06 -0300

    ➕refator: update video service implement

--- src/main/java/com/hleme/apivideos/service/VideoServiceImp.java
+++ src/main/java/com/hleme/apivideos/service/VideoServiceImp.java
@@ -1,9 +1,16 @@
 package com.hleme.apivideos.service;
 
+import com.hleme.apivideos.mapper.VideoMapper;
+import com.hleme.apivideos.repository.VideoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class VideoServiceImp implements VideoService {
+
+    private final VideoMapper videoMapper;
+    private final VideoRepository videoRepository;
+
+
 }
