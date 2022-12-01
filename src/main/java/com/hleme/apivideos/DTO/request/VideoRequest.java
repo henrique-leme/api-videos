2cadd8c304ed500acc89b59de9c8a8dedcb6a1ed
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:00:15 -0300

    ➕refactor: update videorequest

--- src/main/java/com/hleme/apivideos/DTO/request/VideoRequest.java
+++ src/main/java/com/hleme/apivideos/DTO/request/VideoRequest.java
@@ -1,4 +1,17 @@
 package com.hleme.apivideos.DTO.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
 public class VideoRequest {
+
+    private String title;
+
+    private String description;
+
+    private String url;
 }
