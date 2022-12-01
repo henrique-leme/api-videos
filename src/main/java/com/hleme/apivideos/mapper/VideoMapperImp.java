3b4eb76200449fb83389c8068e292bbb9a6af90b
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:16:03 -0300

    ✨feat: creato videoResponse mapper

--- src/main/java/com/hleme/apivideos/mapper/VideoMapperImp.java
+++ src/main/java/com/hleme/apivideos/mapper/VideoMapperImp.java
@@ -1,5 +1,7 @@
 package com.hleme.apivideos.mapper;
 
+import com.hleme.apivideos.DTO.response.VideoResponse;
+import com.hleme.apivideos.model.Video;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -8,4 +10,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class VideoMapperImp implements  VideoMapper{
     private final ModelMapper mapper;
+
+    @Override
+    public VideoResponse toVideoResponse(Video video) {
+        return mapper.map(video, VideoResponse.class);
+    }
 }
