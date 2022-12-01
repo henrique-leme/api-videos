fdb46e319385dd6109ea56a9a55131b7655c9310
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:18:03 -0300

    ✨feat: add mergeVideo mapper

--- src/main/java/com/hleme/apivideos/mapper/VideoMapperImp.java
+++ src/main/java/com/hleme/apivideos/mapper/VideoMapperImp.java
@@ -4,6 +4,7 @@ import com.hleme.apivideos.DTO.request.VideoRequest;
 import com.hleme.apivideos.DTO.response.VideoResponse;
 import com.hleme.apivideos.model.Video;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +17,16 @@ public class VideoMapperImp implements  VideoMapper{
     public VideoResponse toVideoResponse(Video video) {
         return mapper.map(video, VideoResponse.class);
     }
+
     @Override
     public Video fromVideoRequest(VideoRequest videoRequest) {
         return mapper.map(videoRequest, Video.class);
+    }
+
+    @Override
+    public Video mergeVideo(VideoRequest source, Video destination) {
+        mapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+        mapper.map(fromVideoRequest(source), destination);
+        return destination;
     }
 }
