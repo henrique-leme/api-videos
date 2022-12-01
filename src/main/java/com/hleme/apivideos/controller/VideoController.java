a7b7b88b03b4afa35051e914e3f500987e5a7bbf
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:59:23 -0300

    ✨feat: create findall method

--- src/main/java/com/hleme/apivideos/controller/VideoController.java
+++ src/main/java/com/hleme/apivideos/controller/VideoController.java
@@ -1,4 +1,26 @@
 package com.hleme.apivideos.controller;
 
+import com.hleme.apivideos.DTO.response.VideoResponse;
+import com.hleme.apivideos.service.VideoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/videos")
 public class VideoController {
+
+    private final VideoService videoService;
+    
+    @GetMapping
+    public ResponseEntity<Page<VideoResponse>> findAll(String title,@PageableDefault(size = 10) Pageable pageable) {
+            Page<VideoResponse> pageVideoResponse;
+            pageVideoResponse = videoService.findAll(pageable);
+        return ResponseEntity.ok(pageVideoResponse);
+    }
 }
