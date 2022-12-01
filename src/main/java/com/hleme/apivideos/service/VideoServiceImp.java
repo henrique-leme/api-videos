f7869afe5e0b053e75fe12dfcef7447a93c18556
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:21:20 -0300

    ✨feat: add findall method

--- src/main/java/com/hleme/apivideos/service/VideoServiceImp.java
+++ src/main/java/com/hleme/apivideos/service/VideoServiceImp.java
@@ -1,8 +1,10 @@
 package com.hleme.apivideos.service;
 
+import com.hleme.apivideos.DTO.response.VideoResponse;
 import com.hleme.apivideos.mapper.VideoMapper;
 import com.hleme.apivideos.repository.VideoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +14,9 @@ public class VideoServiceImp implements VideoService {
     private final VideoMapper videoMapper;
     private final VideoRepository videoRepository;
 
-
+    @Override
+    public Page<VideoResponse> findAll(Pageable pageable) {
+        return videoRepository.findAll(pageable)
+                .map(videoMapper::toVideoResponse);
+    }
 }
