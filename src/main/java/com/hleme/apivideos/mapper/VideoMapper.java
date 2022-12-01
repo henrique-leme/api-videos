dcc9d173cd21de81cb62de11ed476daa6889fdab
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:06:59 -0300

    ➕refactor: update videomapper

--- src/main/java/com/hleme/apivideos/mapper/VideoMapper.java
+++ src/main/java/com/hleme/apivideos/mapper/VideoMapper.java
@@ -1,4 +1,14 @@
 package com.hleme.apivideos.mapper;
 
+import com.hleme.apivideos.DTO.request.VideoRequest;
+import com.hleme.apivideos.DTO.response.VideoResponse;
+import com.hleme.apivideos.model.Video;
+
 public interface VideoMapper {
+
+    VideoResponse toVideoResponse(Video video);
+
+    Video fromVideoRequest(VideoRequest videoRequest);
+
+    Video mergeVideo(VideoRequest source, Video destination);
 }
