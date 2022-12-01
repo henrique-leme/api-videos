e3d641ffb1b2110473bb6fdd8dde29a9ff8e8a65
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-01 20:03:24 -0300

    ➕refactor: update video response

--- src/main/java/com/hleme/apivideos/DTO/response/VideoResponse.java
+++ src/main/java/com/hleme/apivideos/DTO/response/VideoResponse.java
@@ -1,4 +1,15 @@
 package com.hleme.apivideos.DTO.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class VideoResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private String url;
 }
