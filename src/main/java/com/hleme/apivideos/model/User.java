1eb1a9dcab952f57710d602b8cbe63263696d712
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-30 23:38:52 -0300

    ➕refactor: add annotations

--- src/main/java/com/hleme/apivideos/model/User.java
+++ src/main/java/com/hleme/apivideos/model/User.java
@@ -1,4 +1,16 @@
 package com.hleme.apivideos.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 }
