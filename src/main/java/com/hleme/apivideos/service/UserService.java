ac066e203362389dbd05b444bb0ec5d1e7219bda
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-30 23:43:01 -0300

    ➕refactor: update user service

--- src/main/java/com/hleme/apivideos/service/UserService.java
+++ src/main/java/com/hleme/apivideos/service/UserService.java
@@ -1,4 +1,9 @@
 package com.hleme.apivideos.service;
 
+import com.hleme.apivideos.DTO.response.UserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface UserService {
+    Page<UserResponse> fetchAll(Pageable pageable);
 }
