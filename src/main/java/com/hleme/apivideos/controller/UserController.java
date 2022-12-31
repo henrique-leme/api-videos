6c8f5504b24ff61f1264f755016de0d16aa36014
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-31 00:10:06 -0300

    ✨feat: create find all method

--- src/main/java/com/hleme/apivideos/controller/UserController.java
+++ src/main/java/com/hleme/apivideos/controller/UserController.java
@@ -1,6 +1,13 @@
 package com.hleme.apivideos.controller;
 
+import com.hleme.apivideos.DTO.response.UserResponse;
+import com.hleme.apivideos.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponse>> fetchAll(@PageableDefault(size = 5) Pageable pageable) {
+        return ResponseEntity.ok(userService.fetchAll(pageable));
+    }
 }
