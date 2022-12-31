6329175140680edcd16cd7fdcd6bc0d8ca65b1fa
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-31 00:04:31 -0300

    ➕refactor: fix imports

--- src/main/java/com/hleme/apivideos/mapper/UserMapper.java
+++ src/main/java/com/hleme/apivideos/mapper/UserMapper.java
@@ -1,5 +1,9 @@
 package com.hleme.apivideos.mapper;
 
+import com.hleme.apivideos.DTO.request.UserRequest;
+import com.hleme.apivideos.DTO.response.UserResponse;
+import com.hleme.apivideos.model.User;
+
 public interface UserMapper {
     User fromUserRequest(UserRequest userRequest);
 
