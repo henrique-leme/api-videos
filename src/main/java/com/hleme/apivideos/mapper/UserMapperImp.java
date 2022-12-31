b23dbfb683be37b6c74ab52275d91207ac2f2642
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-31 00:04:48 -0300

    ➕refactor: update user mapper imp

--- src/main/java/com/hleme/apivideos/mapper/UserMapperImp.java
+++ src/main/java/com/hleme/apivideos/mapper/UserMapperImp.java
@@ -1,4 +1,25 @@
 package com.hleme.apivideos.mapper;
 
-public class UserMapperImp {
+import com.hleme.apivideos.DTO.request.UserRequest;
+import com.hleme.apivideos.DTO.response.UserResponse;
+import com.hleme.apivideos.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.modelmapper.ModelMapper;
+
+
+@Component
+@RequiredArgsConstructor
+public class UserMapperImp implements UserMapper{
+    private final ModelMapper mapper;
+
+    @Override
+    public User fromUserRequest(UserRequest userRequest) {
+        return mapper.map(userRequest, User.class);
+    }
+
+    @Override
+    public UserResponse toUserResponse(User user) {
+        return mapper.map(user, UserResponse.class);
+    }
 }
