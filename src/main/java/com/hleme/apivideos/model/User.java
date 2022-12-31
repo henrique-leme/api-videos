bf57850d4dea13ced83b1e8cace530f1ae9992f1
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-30 23:40:27 -0300

    ➕refactor: update user

--- src/main/java/com/hleme/apivideos/model/User.java
+++ src/main/java/com/hleme/apivideos/model/User.java
@@ -3,6 +3,8 @@ package com.hleme.apivideos.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 
 @Getter
 @Setter
@@ -13,4 +15,18 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private boolean enabled;
+    private LocalDateTime deletedAt;
 }
