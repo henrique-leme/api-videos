c4c2783ed0dfcd821f2fd08fe47fc3f308e399f0
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-31 00:09:25 -0300

    ➕refactor: update user service imp

--- src/main/java/com/hleme/apivideos/service/UserServiceImp.java
+++ src/main/java/com/hleme/apivideos/service/UserServiceImp.java
@@ -1,8 +1,21 @@
 package com.hleme.apivideos.service;
 
+import com.hleme.apivideos.DTO.response.UserResponse;
+import com.hleme.apivideos.mapper.UserMapper;
 import com.hleme.apivideos.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImp implements UserService{
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+
+    @Override
+    public Page<UserResponse> fetchAll(Pageable pageable) {
+        return userRepository.findAll(pageable).map(userMapper::toUserResponse);
+    }
 }
