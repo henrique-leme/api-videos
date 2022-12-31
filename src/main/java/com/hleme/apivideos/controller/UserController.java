3cef0529ea32e42518e21a583959124df4d64b18
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-30 23:31:03 -0300

    ➕refactor: update user controller

--- src/main/java/com/hleme/apivideos/controller/UserController.java
+++ src/main/java/com/hleme/apivideos/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.hleme.apivideos.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
 }
