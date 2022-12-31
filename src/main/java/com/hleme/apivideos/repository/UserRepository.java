c70e05f11826481746a11cdbe2cba97e17f8eaae
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-12-30 23:46:09 -0300

    ➕refactor: update java repository

--- src/main/java/com/hleme/apivideos/repository/UserRepository.java
+++ src/main/java/com/hleme/apivideos/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.hleme.apivideos.repository;
 
-public interface UserRepository {
+import com.hleme.apivideos.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
